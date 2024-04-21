d2d06e7d02324898df40c25f6104f9dcb12c3c56
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2024-04-21 18:30:59 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (22.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.rs
+++ 0009-palindrome-number/0009-palindrome-number.rs
@@ -4,14 +4,22 @@ impl Solution {
             return false;
         }
 
+        if x < 10 {
+            return true;
+        }
+
+        if x % 10 == 0 && x != 0 {
+            return false;
+        }
+
         let mut original = x;
         let mut reversed = 0;
 
-        while original > 0 {
+        while original > reversed {
             reversed = reversed * 10 + original % 10;
             original /= 10;
         }
 
-        x == reversed
+        original == reversed || original == reversed / 10
     }
 }
