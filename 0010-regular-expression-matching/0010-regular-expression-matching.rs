fe7887d060cb44daf9163e6d9014dfb95cfa8996
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2024-04-24 21:56:54 +0800

    Time: 0 ms (100.00%), Space: 2 MB (83.80%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        let m = s.len();
+        let n = p.len();
+        let s = s.as_bytes();
+        let p = p.as_bytes();
+
+        let mut dp = vec![vec![false; n + 1]; m + 1];
+        dp[0][0] = true;
+
+        for j in 1..=n {
+            if p[j - 1] == b'*' {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for i in 1..=m {
+            for j in 1..=n {
+                if p[j - 1] == b'.' {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if p[j - 1] == b'*' {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == b'.'));
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1]);
+                }
+            }
+        }
+
+        dp[m][n]
+    }
+}
