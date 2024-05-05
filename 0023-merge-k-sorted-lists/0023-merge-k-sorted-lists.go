1290e54e1e6b74b78aaa02c81ee69a82fd298cb4
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2024-05-05 15:45:24 +0800

    Time: 3 ms (96.86%), Space: 4.7 MB (94.64%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
@@ -1,55 +1,43 @@
-import (
-    "container/heap"
-    // Assume 'precompiled' is the alias for the package where ListNode is defined
-    precompiled "leetcode/precompiled"
-)
+import precompiled "leetcode/precompiled"
 
-// MinHeap is a min-heap of ListNodes.
-type MinHeap []*precompiled.ListNode
-
-func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i].Val < h[j].Val }
-func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *MinHeap) Push(x interface{}) {
-    *h = append(*h, x.(*precompiled.ListNode))
-}
-
-func (h *MinHeap) Pop() interface{} {
-    old := *h
-    n := len(old)
-    x := old[n-1]
-    *h = old[0 : n-1]
-    return x
-}
-
-// mergeKLists merges k sorted linked-lists into one sorted linked-list.
-func mergeKLists(lists []*precompiled.ListNode) *precompiled.ListNode {
-    minHeap := &MinHeap{}
-    heap.Init(minHeap)
-
-    // Initialize the min heap with the head of each list.
-    for _, list := range lists {
-        if list != nil {
-            heap.Push(minHeap, list)
-        }
-    }
-
-    // Create a dummy head for the result list.
+// mergeTwoLists merges two sorted linked-lists into one sorted linked-list.
+func mergeTwoLists(l1, l2 *precompiled.ListNode) *precompiled.ListNode {
     dummy := &precompiled.ListNode{}
     current := dummy
 
-    // Extract the smallest element from the heap and add it to the result list.
-    for minHeap.Len() > 0 {
-        smallest := heap.Pop(minHeap).(*precompiled.ListNode)
-        current.Next = smallest
-        current = current.Next
-
-        // If there is a next element in the list, add it to the heap.
-        if smallest.Next != nil {
-            heap.Push(minHeap, smallest.Next)
+    for l1 != nil && l2 != nil {
+        if l1.Val < l2.Val {
+            current.Next = l1
+            l1 = l1.Next
+        } else {
+            current.Next = l2
+            l2 = l2.Next
         }
+        current = current.Next
+    }
+
+    if l1 != nil {
+        current.Next = l1
+    } else {
+        current.Next = l2
     }
 
     return dummy.Next
+}
+
+// mergeKLists merges k sorted linked-lists into one sorted linked-list using divide and conquer.
+func mergeKLists(lists []*precompiled.ListNode) *precompiled.ListNode {
+    if len(lists) == 0 {
+        return nil
+    }
+
+    interval := 1
+    for interval < len(lists) {
+        for i := 0; i < len(lists)-interval; i += interval * 2 {
+            lists[i] = mergeTwoLists(lists[i], lists[i+interval])
+        }
+        interval *= 2
+    }
+
+    return lists[0]
 }
