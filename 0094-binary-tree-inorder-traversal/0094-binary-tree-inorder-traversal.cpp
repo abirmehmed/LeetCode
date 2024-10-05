ca2049833d89573f6b1fc752eb7ac696de149d66
Abir Mehmed <124920759+abirmehmed@users.noreply.github.com>
2024-10-05 09:22:11 -0700

    Time: 0 ms (100.00%), Space: 10.1 MB (30.75%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> stk;
+        TreeNode* current = root;
+
+        while (current != nullptr || !stk.empty()) {
+            // Reach the leftmost node of the current node
+            while (current != nullptr) {
+                stk.push(current);
+                current = current->left;
+            }
+
+            // Current must be null at this point, so we process the top of the stack
+            current = stk.top();
+            stk.pop();
+            result.push_back(current->val); // Visit the node
+
+            // Now we need to visit the right subtree
+            current = current->right;
+        }
+
+        return result;
+    }
+};
